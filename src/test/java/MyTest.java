375687d8ad304ea59fe975007af0df65fe50d992
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-16 13:12:44 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testSomething() {
+        assertEquals("1 sheep...", Kata.countingSheep(1));
+        assertEquals("1 sheep...2 sheep...", Kata.countingSheep(2));
+        assertEquals("1 sheep...2 sheep...3 sheep...", Kata.countingSheep(3));
+    }
 
+    private static class Kata {
+        public static String countingSheep(int num) {
+            String text = " sheep...";
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= num; i++) {
+                result.append(i).append(text);
+            }
+            return result.toString();
+        }
     }
 }
